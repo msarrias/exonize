c4815abd2f288fa2dd1b98c56db89d9932221a5e
msarrias <marina.sarrias@math.su.se>
2022-10-31 14:49:38 +0100

    refactor

--- data_base_op.py
+++ data_base_op.py
@@ -1,29 +1,46 @@
 import gffutils # for creating/loading DBs
+import subprocess # for calling gffread
 
-
-class data_base_op(object):
+class DataBaseOp(object):
     def __init__(self,
                  db_path,
                  in_file_path='',
                  create_db=False,
                  create_introns=False):
+        self.db = None
         self.db_path = db_path
         self.create_introns = create_introns
         if create_db:
             self.in_file_path = in_file_path
+            if 'gtf' in self.in_file_path:
+                self.old_filename = self.in_file_path
+                self.in_file_path = (self.old_filename.rsplit('.gtf')[0]
+                                     + '.gff')
+                self.convert_gtf_to_gff()
+                print('the GTF file has been converted into a GFF3 file')
+                print(f'with filename: {self.in_file_path}')
             self.create_database()
-            if self.create_introns == True:
+            if self.create_introns:
                 self.load_db()
                 self.get_introns()
-                
-                
+               
+            
+    def convert_gtf_to_gff(self):
+        gffread_command = [ "gffread",
+                           self.old_filename, 
+                           "-O", "-o", self.in_file_path
+                          ]
+        subprocess.call(gffread_command)
+        
+        
     def create_database(self):
         try:
             gffutils.create_db(self.in_file_path,
                                dbfn=self.db_path,
                                force = True, keep_order=True,
                                merge_strategy='merge',
-                               sort_attribute_values=True)
+                               sort_attribute_values=True,
+                               )
         except ValueError:
             print("Wrong infile path")
 
