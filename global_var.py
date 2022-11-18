99a783160270290c31e5b8fc51114921d1b348e3
msarrias <marina.sarrias@math.su.se>
2022-11-18 12:27:35 +0100

    remove directories info

--- global_var.py
+++
@@ -1,4 +0,0 @@
-FASTA_DIRECT = '/proj/snic2022-22-447/msarrias/projects/exon_dups_analysis/code/00-fasta-files/'
-DB_DIRECT = '/proj/snic2022-22-447/msarrias/dbs/'
-PKL_DIRECT = '/proj/snic2022-22-447/msarrias/projects/exon_dups_analysis/code/01-pkl-files/'
-MINIPROT_DIR = "/domus/h1/msarrias/bin/miniprot/miniprot"
