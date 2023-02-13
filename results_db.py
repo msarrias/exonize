e37a75efc68a0473902adcc8ab9e855a63a2e9b0
msarrias <marina.sarrias@math.su.se>
2023-02-13 14:07:25 +0100

    redefined the method for getting ungapped sequences and counting stop codons, included a new column for the HSP attribute

--- results_db.py
+++ results_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import re # regular expressions
 
 
 class SQlite_res():
@@ -32,6 +33,10 @@ class SQlite_res():
     @staticmethod
     def hd_amino_seqs(seq_a, seq_b):
         return sum([seq_a[i:i+3] != seq_b[i:i+3] for i in range(0,len(seq_a), 3)])
+    
+    @staticmethod
+    def ungap_sequence(seq):
+        return re.sub(r'[^\w]', '', seq)
 
     
     def create_table_concatenate_fragments(self):
@@ -39,11 +44,7 @@ class SQlite_res():
         cursor = conn.cursor()
         new_table = """
         CREATE TABLE IF NOT EXISTS concatenate_fragments AS
-        SELECT *,
-        CAST(query_ungapped_dna_seq_length as REAL)/query_length AS query_aligned_fraction
-        FROM(
-        SELECT *, 
-        LENGTH(query_ungapped_dna_sequence) AS query_ungapped_dna_seq_length
+        SELECT * 
         FROM (
         SELECT 
         gene_id,
@@ -55,19 +56,21 @@ class SQlite_res():
         target_end,
         query_end - query_start as query_length,
         target_end - target_start as target_length,
+        HSP_n,
+        MAX(score) AS score,
         MAX(category) AS category,
-        MAX(is_pseudo_exon) AS is_pseudo_exon,
+        MAX(strand) AS strand,
+        GROUP_CONCAT(query_frame, ';') AS query_frame,
+        GROUP_CONCAT(hit_frame, ';') AS hit_frame,
+        SUM(n_stop_codons) AS n_stop_codons,
         GROUP_CONCAT(frag_query_start, ';') AS frag_query_start,
         GROUP_CONCAT(frag_query_end, ';') AS frag_query_end,
         GROUP_CONCAT(frag_target_start, ';') AS frag_hit_start,
         GROUP_CONCAT(frag_target_end, ';') AS frag_hit_end,
-        GROUP_CONCAT(query_dna_sequence, '') AS query_dna_sequence, 
-        replace(replace(replace(replace(replace(query_dna_sequence,'<',''),
-        '>',''), '*',''), '-',''), '#', '') 
-        AS query_ungapped_dna_sequence,
-        GROUP_CONCAT(target_dna_sequence, '') AS target_dna_sequence,
-        GROUP_CONCAT(query_prot_sequence, '') AS query_prot_sequence,
-        GROUP_CONCAT(target_prot_sequence, '') AS target_prot_sequence
+        GROUP_CONCAT(query_aln_dna_sequence, '') AS query_aln_dna_sequence, 
+        GROUP_CONCAT(target_aln_dna_sequence, '') AS target_aln_dna_sequence,
+        GROUP_CONCAT(query_aln_prot_sequence, '') AS query_aln_prot_sequence,
+        GROUP_CONCAT(target_aln_prot_sequence, '') AS target_aln_prot_sequence
         FROM (
         SELECT * 
         FROM Fragments
@@ -75,12 +78,14 @@ class SQlite_res():
         gene_id ASC,
         query_id ASC,
         target_id ASC,
+        HSP_n ASC,
         query_start ASC,
         query_end ASC)
         GROUP BY 
         gene_id, 
         query_id, 
-        target_id));
+        target_id,
+        HSP_n);
         """
         #create table
         cursor.execute(new_table)
@@ -93,42 +98,67 @@ class SQlite_res():
     def insert_conservation_percentage_columns(self):
         conn = sqlite3.connect(self.db_path)
         cur = conn.cursor()
-        if 'dna_perc_conservation' and 'prot_perc_conservation' not in self.concatenate_fragments_colnames:
-            cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN dna_perc_conservation REAL;")
-            cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN prot_perc_conservation REAL;")
-            #populate column
-            cur.execute("""
-            SELECT 
-            gene_id,
-            query_id,
-            target_id,
-            query_dna_sequence,
-            target_dna_sequence,
-            query_prot_sequence,
-            target_prot_sequence
-            FROM
+        cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN dna_perc_conservation REAL;")
+        cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN prot_perc_conservation REAL;")
+        cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN query_dna_sequence VARCHAR;")
+        cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN target_dna_sequence VARCHAR;")
+        cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN query_prot_sequence VARCHAR;")
+        cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN target_prot_sequence VARCHAR;")
+        cur.execute("ALTER TABLE concatenate_fragments ADD COLUMN query_aligned_fraction REAL;")
+        #populate column
+        cur.execute("""
+        SELECT 
+        gene_id,
+        query_id,
+        target_id,
+        query_length,
+        query_aln_dna_sequence,
+        target_aln_dna_sequence,
+        query_aln_prot_sequence,
+        target_aln_prot_sequence,
+        HSP_n,
+        score
+        FROM
+        concatenate_fragments
+        """)
+        for row in cur:
+            cursor_insert = conn.cursor()
+            (gene_id, 
+             query_id, 
+             target_id,
+             query_length,
+             query_aln_dna_sequence,
+             target_aln_dna_sequence,
+             query_aln_prot_sequence,
+             target_aln_prot_sequence,
+             HSP_n,
+             score) = row
+            dna_consev = 1 - (self.hd_dna_seqs(query_aln_dna_sequence, target_aln_dna_sequence) / len(query_aln_dna_sequence))
+            prot_consev = 1 - (self.hd_amino_seqs(query_aln_prot_sequence, target_aln_prot_sequence) / len(query_aln_prot_sequence))
+            query_dna_sequence = self.ungap_sequence(query_aln_dna_sequence)
+            target_dna_sequence = self.ungap_sequence(target_aln_dna_sequence)
+            query_prot_sequence = self.ungap_sequence(query_aln_prot_sequence)
+            target_prot_sequence = self.ungap_sequence(target_aln_prot_sequence)
+            query_aligned_fraction = len(query_dna_sequence)/query_length
+            update_string = """
+            UPDATE 
             concatenate_fragments
-            """)
-            for row in cur:
-                cursor_insert = conn.cursor()
-                (gene_id, 
-                query_id, 
-                target_id,
-                query_dna_sequence,
-                target_dna_sequence,
-                query_prot_sequence,
-                target_prot_sequence) = row
-                dna_consev = 1 - (self.hd_dna_seqs(query_dna_sequence, target_dna_sequence) / len(query_dna_sequence) )
-                prot_consev = 1 - (self.hd_amino_seqs(query_prot_sequence, target_prot_sequence) / len(query_prot_sequence))
-                update_string = """
-                UPDATE 
-                concatenate_fragments
-                SET 
-                dna_perc_conservation = ?,
-                prot_perc_conservation = ?
-                WHERE 
-                (gene_id = ? AND query_id = ? AND target_id = ?)
-                """
-                cursor_insert.execute(update_string, (dna_consev, prot_consev, gene_id, query_id, target_id))
-            conn.commit()
-            conn.close()
+            SET 
+            dna_perc_conservation = ?,
+            prot_perc_conservation = ?,
+            query_dna_sequence = ?,
+            target_dna_sequence = ?,
+            query_prot_sequence = ?,
+            target_prot_sequence = ?,
+            query_aligned_fraction = ?
+            WHERE 
+            (gene_id = ? AND query_id = ? AND target_id = ? AND HSP_n = ? AND score = ?)
+            """
+            cursor_insert.execute(update_string, (dna_consev, prot_consev,
+                                                  query_dna_sequence, target_dna_sequence,
+                                                  query_prot_sequence, target_prot_sequence,
+                                                  query_aligned_fraction,
+                                                  gene_id, query_id, target_id,
+                                                  HSP_n, score))
+        conn.commit()
+        conn.close()
