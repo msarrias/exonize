f8edbe3777017cbdcb6af3539733c784ddb7fd8d
msarrias <marina.sarrias@math.su.se>
2023-07-24 12:30:40 +0900

    rearranged the functions depending on whether they are for creating/inserting or quering a table. Created a function for creating a full length events table, MXEs events and obligatory events.

--- sqlite_utils.py
+++ sqlite_utils.py
@@ -42,65 +42,61 @@ def connect_create_results_db(db_path, timeout_db) -> None:
     query_num_stop_codons INTEGER NOT NULL,
     target_num_stop_codons INTEGER NOT NULL,
     dna_perc_identity REAL NOT NULL,
-    prot_perc_identity REAL NOT NULL)
+    prot_perc_identity REAL NOT NULL,
+    UNIQUE(fragment_id, gene_id, CDS_start, CDS_end, query_start, query_end, target_start, target_end))
     """)
-    # cursor.execute("""
-    # CREATE TABLE IF NOT EXISTS Fragments_matches (
-    # fragment_match_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    # fragment_id INTEGER NOT NULL REFERENCES Fragments(fragment_id),
-    # gene_id VARCHAR(100) NOT NULL REFERENCES Genes(gene_id),
-    # mrna_id VARCHAR(100) NOT NULL,
-    # CDS_start INTEGER NOT NULL,
-    # CDS_end INTEGER NOT NULL,
-    # Neither BINARY(1) NOT NULL,
-    # query BINARY(1) NOT NULL,
-    # target BINARY(1) NOT NULL,
-    # Both BINARY(1) NOT NULL,
-    # UNIQUE(fragment_id, gene_id, mrna_id, CDS_start, CDS_end))""")
-    # cursor.execute("""
-    # CREATE INDEX IF NOT EXISTS Fragments_id_idx ON Fragments_matches (gene_id, mrna_id);
-    # """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Full_length_duplications (
+    fragment_match_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fragment_id INTEGER NOT NULL REFERENCES Fragments(fragment_id),
+    gene_id VARCHAR(100) NOT NULL REFERENCES Genes(gene_id),
+    mrna_id VARCHAR(100) NOT NULL,
+    CDS_start INTEGER NOT NULL,
+    CDS_end INTEGER NOT NULL,
+    neither BINARY(1) NOT NULL,
+    query BINARY(1) NOT NULL,
+    target BINARY(1) NOT NULL,
+    both BINARY(1) NOT NULL,
+    UNIQUE(fragment_id, gene_id, mrna_id, CDS_start, CDS_end))""")
+    cursor.execute("""
+    CREATE INDEX IF NOT EXISTS Full_length_duplications_idx ON Full_length_duplications (fragment_id, gene_id, mrna_id);
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS MXEs_events (
+        event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        gene_id VARCHAR(100) NOT NULL REFERENCES Genes(gene_id),
+        mrna_A VARCHAR(100) NOT NULL,
+        mrna_B VARCHAR(100) NOT NULL,
+        CDS_A VARCHAR(100) NOT NULL,
+        CDS_A_start INTEGER NOT NULL,
+        CDS_A_end INTEGER NOT NULL,
+        CDS_B VARCHAR(100) NOT NULL,
+        CDS_B_start INTEGER NOT NULL,
+        CDS_B_end INTEGER NOT NULL,
+        UNIQUE(gene_id, mrna_A, mrna_B))
+        """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Obligatory_events (
+        event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        fragment_id INTEGER NOT NULL REFERENCES Fragments(fragment_id),
+        gene_id VARCHAR(100) NOT NULL REFERENCES Genes(gene_id),
+        mrna_id VARCHAR(100) NOT NULL,
+        mrna_start INTEGER NOT NULL,
+        mrna_end INTEGER NOT NULL,
+        query_CDS_start INTEGER NOT NULL,
+        query_CDS_end INTEGER NOT NULL,
+        query_CDS_id VARCHAR(100) NOT NULL,
+        query_start INTEGER NOT NULL,
+        query_end INTEGER NOT NULL,
+        target_CDS_id VARCHAR(100) NOT NULL,
+        target_CDS_start INTEGER NOT NULL,
+        target_CDS_end INTEGER NOT NULL,
+        target_start INTEGER NOT NULL,
+        target_end INTEGER NOT NULL,
+        UNIQUE(fragment_id, gene_id, mrna_id, query_CDS_id, target_CDS_id))
+        """)
     db.commit()
     db.close()
-
-
-def create_mutually_exclusive_events_table(db_path, timeout_db) -> None:
-    db = sqlite3.connect(db_path, timeout=timeout_db)
-    cursor = db.cursor()
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS MXEs_events (
-    event_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    gene_id VARCHAR(100) NOT NULL REFERENCES Genes(gene_id),
-    mrna_A VARCHAR(100) NOT NULL,
-    mrna_B VARCHAR(100) NOT NULL,
-    CDS_A VARCHAR(100) NOT NULL,
-    CDS_A_start INTEGER NOT NULL,
-    CDS_A_end INTEGER NOT NULL,
-    CDS_B VARCHAR(100) NOT NULL,
-    CDS_B_start INTEGER NOT NULL,
-    CDS_B_end INTEGER NOT NULL,
-    UNIQUE(gene_id, mrna_A, mrna_B))
-    """)
-
-
-def create_obligatory_events_table(db_path, timeout_db) -> None:
-    db = sqlite3.connect(db_path, timeout=timeout_db)
-    cursor = db.cursor()
-    cursor.execute("""
-            CREATE TABLE IF NOT EXISTS Obligatory_events (
-            event_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            gene_id VARCHAR(100) NOT NULL REFERENCES Genes(gene_id),
-            mrna_id VARCHAR(100) NOT NULL,
-            mrna_start INTEGER NOT NULL,
-            mrna_end INTEGER NOT NULL,
-            CDS_A VARCHAR(100) NOT NULL,
-            CDS_A_start INTEGER NOT NULL,
-            CDS_A_end INTEGER NOT NULL,
-            CDS_B VARCHAR(100) NOT NULL,
-            CDS_B_start INTEGER NOT NULL,
-            CDS_B_end INTEGER NOT NULL,
-            UNIQUE(gene_id, mrna_id, CDS_A, CDS_B))
-            """)
 
 
 def query_gene_ids_in_res_db(db_path, timeout_db) -> list:
@@ -113,38 +109,6 @@ def query_gene_ids_in_res_db(db_path, timeout_db) -> list:
 
 
 # #### INSERT INTO TABLES #####
-def insert_fragments_matches_table(db_path, timeout_db, tuples_list: list) -> None:
-    db = sqlite3.connect(db_path, timeout=timeout_db)
-    cursor = db.cursor()
-    insert_frag_match_table_param = """
-     INSERT OR IGNORE INTO Fragments_matches (
-     gene_id,
-     CDS_mrna_id,
-     CDS_mrna_start,
-     CDS_mrna_end,
-     CDS_id,
-     CDS_start,
-     CDS_end,
-     query_start,
-     query_end,
-     target_start,
-     target_end,
-     match_mrna_id,
-     match_mrna_start,
-     match_mrna_end,
-     match_id,
-     feature,
-     match_annot_start,
-     match_annot_end,
-     overlap_percentage_CDS,
-     overlap_percentage_match)
-     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-     """
-    cursor.executemany(insert_frag_match_table_param, tuples_list)
-    db.commit()
-    db.close()
-
-
 def insert_gene_ids_table(db_path, timeout_db, gene_args_tuple: tuple) -> None:
     db = sqlite3.connect(db_path, timeout=timeout_db)
     cursor = db.cursor()
@@ -206,30 +170,77 @@ def insert_fragments_calls():
     return insert_fragments_table_param, insert_gene_table_param
 
 
+def instert_full_length_event(db_path, timeout_db, tuples_list):
+    db = sqlite3.connect(db_path, timeout=timeout_db)
+    cursor = db.cursor()
+    insert_full_length_event_table_param = """
+    INSERT INTO Full_length_duplications (
+    fragment_id,
+    gene_id,
+    mrna_id,
+    CDS_start,
+    CDS_end,
+    neither,
+    query,
+    target,
+    both )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) 
+    """
+    cursor.executemany(insert_full_length_event_table_param, tuples_list)
+    db.commit()
+    db.close()
+
+
+def instert_obligatory_event(db_path, timeout_db, tuples_list):
+    db = sqlite3.connect(db_path, timeout=timeout_db)
+    cursor = db.cursor()
+    insert_obl_event_table_param = """
+     INSERT OR IGNORE INTO Obligatory_events (
+     fragment_id,
+     gene_id,
+     mrna_id,
+     mrna_start,
+     mrna_end,
+     query_CDS_start,
+     query_CDS_end,
+     query_CDS_id,
+     query_start,
+     query_end,
+     target_CDS_id,
+     target_CDS_start,
+     target_CDS_end,
+     target_start,
+     target_end)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+     """
+    cursor.executemany(insert_obl_event_table_param, tuples_list)
+    db.commit()
+    db.close()
+
+
 # ###### QUERY TABLES ######
-def query_within_gene_events(db_path, timeout_db, gene_id: str) -> list:
+def query_within_gene_events(db_path: str, timeout_db: float) -> list:
     db = sqlite3.connect(db_path, timeout=timeout_db)
     cursor = db.cursor()
     fragments_query = """
     SELECT 
-    mrna_id,
-    CDS_id,
-    CDS_start,
-    CDS_end,
-    query_start,
-    query_end,
-    target_start,
-    target_end,
-    MIN(evalue)
-    FROM Fragments
-    WHERE gene_id==?
-    GROUP BY gene_id, mrna_id, CDS_id
-    HAVING (ABS(query_start - target_start) > 5 OR ABS(query_end - target_end) > 5)
+        f.fragment_id,
+        f.gene_id,
+        g.gene_start,
+        g.gene_end,
+        f.CDS_start,
+        f.CDS_end,
+        f.query_start,
+        f.query_end ,
+        f.target_start ,
+        f.target_end 
+    FROM Fragments as f
+    JOIN Genes AS g ON g.gene_id = f.gene_id
     """
-    cursor.execute(fragments_query, (gene_id,))
+    cursor.execute(fragments_query)
     records = cursor.fetchall()
     db.close()
-    return records
+    return [i for i in records]
 
 
 def query_genes_with_duplicated_cds(db_path, timeout_db) -> list:
