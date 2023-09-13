5c97a1b527cd3555073aac452fd380e052b06de8
msarrias <marina.sarrias@math.su.se>
2023-09-13 11:48:07 +0200

    included a call to the analysis method

--- exonize.py
+++ exonize.py
@@ -1,4 +1,4 @@
-from exonize_handler import Exonize
+from exonize_handler import *
 import argparse
 
 
@@ -9,8 +9,7 @@ def check_requirements():
         installed = [package.split()[0] for package in installed_packages]
         with open(req_file_name, "r") as f:
             required_packages = f.read().splitlines()
-        missing_packages = [package.split("==")[0] for package in required_packages
-                            if package.split("==")[0] not in installed]
+        missing_packages = [package for package in required_packages if package.split("==")[0] not in installed]
         if missing_packages:
             print("The following packages are missing:")
             for package in missing_packages:
@@ -27,7 +26,7 @@ def argument_parser():
     parser.add_argument('--results_db_name', default='', type=str, help='Results database name')
     parser.add_argument('--spec_attribute', default='ID', type=str, help='Feature reference for writting intron annotations with gffutils')
     parser.add_argument('--save_input_files', action='store_true', help='Saves input and output tblastx files')
-    parser.add_argument('--verbose', default=False, action='store_true', help='Verbose mode')
+    parser.add_argument('--verbose', default=True, action='store_true', help='Verbose mode')
     parser.add_argument('--hard_masking', default=False, action='store_true', help='Hard masking flag')
     parser.add_argument('--evalue_threshold', default=1e-2, type=float, help='E-value threshold')
     parser.add_argument('--sleep_max_seconds', default=5, type=int, help='Max sleep seconds')
@@ -45,23 +44,24 @@ def argument_parser():
 def main():
     check_requirements()
     args = argument_parser()
-    Exonize(gff_file_path=args.gff_file_path,
-            genome_path=args.genome_path,
-            specie_identifier=args.specie_identifier,
-            results_db_name=args.results_db_name,
-            spec_attribute=args.spec_attribute,
-            save_input_files=args.save_input_files,
-            verbose=args.verbose,
-            hard_masking=args.hard_masking,
-            evalue_threshold=args.evalue_threshold,
-            sleep_max_seconds=args.sleep_max_seconds,
-            min_exon_length=args.min_exon_length,
-            cds_overlapping_threshold=args.cds_overlapping_threshold,
-            masking_perc_threshold=args.masking_perc_threshold,
-            self_hit_threshold=args.self_hit_threshold,
-            batch_number=args.batch_number,
-            threads=args.threads,
-            timeout_db=args.timeout_db)
+    exonize_obj = Exonize(gff_file_path=args.gff_file_path,
+                          genome_path=args.genome_path,
+                          specie_identifier=args.specie_identifier,
+                          results_db_name=args.results_db_name,
+                          spec_attribute=args.spec_attribute,
+                          save_input_files=args.save_input_files,
+                          verbose=args.verbose,
+                          hard_masking=args.hard_masking,
+                          evalue_threshold=args.evalue_threshold,
+                          sleep_max_seconds=args.sleep_max_seconds,
+                          min_exon_length=args.min_exon_length,
+                          cds_overlapping_threshold=args.cds_overlapping_threshold,
+                          masking_perc_threshold=args.masking_perc_threshold,
+                          self_hit_threshold=args.self_hit_threshold,
+                          batch_number=args.batch_number,
+                          threads=args.threads,
+                          timeout_db=args.timeout_db)
+    exonize_obj.run_analysis()
 
 
 if __name__ == '__main__':
