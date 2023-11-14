20a2fe1c4397ad2d82296ffc604588f4b11328f5
msarrias <marina.sarrias@math.su.se>
2023-11-14 14:20:18 +0100

    added test case and fixed tuple formation in the test_full_matches function

--- tests/test_Exonize.py
+++ tests/test_Exonize.py
@@ -10,12 +10,17 @@ test_object = Exonize('',
 
 
 def test_full_matches():
+    # tuple format: (fragment_id, gene_id, query_start, query_end, target_start, target_end, type)
     matches_a = [(1, 'gene_test', 5, 10, 25, 30, 'INS_CDS'),
                  (2, 'gene_test', 20, 35, 0, 15, 'TRUNC'),
                  (3, 'gene_test', 4, 10, 23, 29, 'TRUNC')]
     test_object.cds_overlapping_threshold = 0.8
-    assert test_object.assign_pair_ids(matches_a) == [(1, 1), (1, 2), (1, 3)]
+    assert test_object.assign_pair_ids(matches_a) == [(1, 'MATCH', 1), (1, 'RECIPROCAL', 2), (1, 'OVERLAPPING', 3)]
     matches_b = [(1, 'gene_test', 5, 10, 25, 30, 'FULL'),
                  (2, 'gene_test', 25, 30, 5, 10, 'FULL'),
                  (3, 'gene_test', 4, 10, 23, 29, 'TRUNC')]
-    assert test_object.assign_pair_ids(matches_b) == [(1, 1), (1, 2), (1, 3)]
+    assert test_object.assign_pair_ids(matches_b) == [(1, 'MATCH', 1), (1, 'RECIPROCAL', 2), (1, 'OVERLAPPING', 3)]
+    matches_c = [(1, 'gene_test', 5, 10, 25, 30, 'FULL'),
+                 (2, 'gene_test', 25, 30, 5, 10, 'FULL'),
+                 (3, 'gene_test', 4, 10, 50, 56, 'TRUNC')]
+    assert test_object.assign_pair_ids(matches_c) == [(1, 'MATCH', 1), (1, 'RECIPROCAL', 2), (1, 'NON-RECIPROCAL', 3)]
