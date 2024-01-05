ad91f36cd6f0762783d970afe83ae2bbd655490f
msarrias <marina.sarrias@math.su.se>
2024-01-05 18:47:30 +0100

    specified the constructor arguments in the EnvironmentSetup class, also did some refactoring

--- exonize/environment_setup.py
+++ exonize/environment_setup.py
@@ -7,20 +7,19 @@ from datetime import datetime
 class EnvironmentSetup(object):
     logger: logging.Logger
 
-    def __init__(self, **attributes):
-        self.__FILE_ONLY_INFO = 9
-        for key, value in attributes.items():
-            setattr(self, key, value)
-
-    # noinspection PyProtectedMember
-    def file_only_info(
+    def __init__(
             self,
-            message,
-            *args,
-            **kws
+            hard_force,
+            soft_force,
+            working_directory,
+            results_database_path
     ):
-        if self.logger.isEnabledFor(self.FILE_ONLY_INFO):
-            self.logger._log(self.FILE_ONLY_INFO, message, args, **kws)
+        self.__FILE_ONLY_INFO = 9
+        self.HARD_FORCE = hard_force
+        self.SOFT_FORCE = soft_force
+        self.working_directory = working_directory
+        self.results_database_path = results_database_path
+        self.setup_environment()
 
     def configure_logger(self):
         """
@@ -42,8 +41,8 @@ class EnvironmentSetup(object):
 
         # Define file handler for the "FILE_ONLY_INFO" level
         log_file_name = f"exonize_log_{datetime.now():%Y%m%d_%H%M%S}.log"
-        log_file_name = os.path.join(self.working_dir, log_file_name)
-        file_handler = logging.FileHandler(log_file_name, mode='w')
+        log_file_path = os.path.join(self.working_directory, log_file_name)
+        file_handler = logging.FileHandler(log_file_path, mode='w')
         file_handler.setLevel(self.__FILE_ONLY_INFO)  # Changed level to custom level
         file_handler.addFilter(ExcludeInfoFilter())  # Added filter
         file_handler.setFormatter(logging.Formatter('%(message)s'))
@@ -57,12 +56,22 @@ class EnvironmentSetup(object):
         self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
 
+    # noinspection PyProtectedMember
+    def file_only_info(
+            self,
+            message,
+            *args,
+            **kws
+    ):
+        if self.logger.isEnabledFor(self.__FILE_ONLY_INFO):
+            self.logger._log(self.__FILE_ONLY_INFO, message, args, **kws)
+
     def setup_environment(self):
         if self.HARD_FORCE:
-            if os.path.exists(self.working_dir):
-                shutil.rmtree(self.working_dir)
+            if os.path.exists(self.working_directory):
+                shutil.rmtree(self.working_directory)
         elif self.SOFT_FORCE:
-            if os.path.exists(self.results_db):
-                os.remove(self.results_db)
-        os.makedirs(self.working_dir, exist_ok=True)
+            if os.path.exists(self.results_database_path):
+                os.remove(self.results_database_path)
+        os.makedirs(self.working_directory, exist_ok=True)
         self.configure_logger()
