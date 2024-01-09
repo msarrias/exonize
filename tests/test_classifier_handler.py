cd5cdd227883f2f936f801e36166ed09464f3c3f
msarrias <marina.sarrias@math.su.se>
2024-01-09 17:45:32 +0100

    added test_find_overlapping_annotations test_classifier_handler.py

--- tests/test_classifier_handler.py
+++ tests/test_classifier_handler.py
@@ -1,14 +1,43 @@
 from unittest.mock import Mock
+from exonize.blast_searcher import BLASTsearcher
 from exonize.classifier_handler import ClassifierHandler
+import portion as P
 
+blast_engine = BLASTsearcher(
+    data_container=Mock(),
+    masking_percentage_threshold=0.8,
+    sleep_max_seconds=40,
+    self_hit_threshold=0.5,
+    min_exon_length=20,
+    cds_overlapping_threshold=0.8,
+    evalue_threshold=1e-5,
+    debug_mode=False,
+)
 classifier_handler = ClassifierHandler(
-    blast_engine=Mock(),
+    blast_engine=blast_engine,
     cds_overlapping_threshold=0.8,
 )
 
 
 def test_find_overlapping_annotations():
-    pass
+    transcript_dictionary = {
+        "structure": [
+            {"id": "CDS1", "coordinate": P.open(0, 100), "frame": "0", "type": "CDS"},
+            {"id": "CDS2", "coordinate": P.open(50, 150), "frame": "0", "type": "CDS"},
+            {"id": "CDS3", "coordinate": P.open(200, 300), "frame": "0", "type": "CDS"}
+        ]
+    }
+    cds_coordinate = P.open(75, 125)
+    classifier_handler.cds_overlapping_threshold = 0.0
+    overlapping_annotations = classifier_handler.find_overlapping_annotations(
+        transcript_dictionary=transcript_dictionary,
+        cds_coordinate=cds_coordinate
+    )
+    expected_overlapping_annotations = [
+        ("CDS1", P.open(0, 100), 0),
+        ("CDS2", P.open(50, 150), 0)
+    ]
+    assert overlapping_annotations == expected_overlapping_annotations
 
 
 def test_identify_query():
