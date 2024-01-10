004a3026ce4cc2faa53eba9df693cf76e2248d9c
msarrias <marina.sarrias@math.su.se>
2024-01-10 18:26:14 +0100

    refacto test_build_reference_dictionary - test_counter_handler.py

--- tests/test_counter_handler.py
+++ tests/test_counter_handler.py
@@ -1,9 +1,20 @@
 from unittest.mock import Mock
 from exonize.counter_handler import CounterHandler
+from exonize.blast_searcher import BLASTsearcher
 import portion as P
 
+blast_engine = BLASTsearcher(
+    data_container=Mock(),
+    masking_percentage_threshold=0.8,
+    sleep_max_seconds=40,
+    self_hit_threshold=0.5,
+    min_exon_length=20,
+    cds_overlapping_threshold=0.8,
+    evalue_threshold=1e-5,
+    debug_mode=False,
+)
 counter_handler = CounterHandler(
-    blast_engine=Mock(),
+    blast_engine=blast_engine,
     cds_overlapping_threshold=0.8,
 )
 
@@ -36,34 +47,22 @@ def test_get_overlapping_clusters():
         [(P.open(120, 170), 0.7)]
     ]
 
-    # Case 3: Mixed case
-    target_coordinates_set_3 = {
-        (P.open(0, 50), 0.9),
-        (P.open(40, 90), 0.8),
-        (P.open(100, 150), 0.7)
-    }
-    expected_clusters_3 = [
-        [(P.open(0, 50), 0.9), (P.open(40, 90), 0.8)],
-        [(P.open(100, 150), 0.7)]
-    ]
-
     # Test and assert
     assert counter_handler.get_overlapping_clusters(
-        target_coordinates_set=target_coordinates_set_1
+        target_coordinates_set=target_coordinates_set_1,
+        threshold=0
     ) == expected_clusters_1
     assert counter_handler.get_overlapping_clusters(
-        target_coordinates_set=target_coordinates_set_2
+        target_coordinates_set=target_coordinates_set_2,
+        threshold=0.
     ) == expected_clusters_2
-    assert counter_handler.get_overlapping_clusters(
-        target_coordinates_set=target_coordinates_set_3
-    ) == expected_clusters_3
 
 
 def test_build_reference_dictionary():
     cds_candidates_dictionary = {
         'candidates_cds_coordinates':
             [P.open(0, 100),
-             P.open(200, 300)]
+             P.open(200, 250)]
     }
     clusters_list = [
         [(P.open(0, 50), 0.9), (P.open(40, 90), 0.8)],
@@ -73,15 +72,15 @@ def test_build_reference_dictionary():
 
     expected_output = {
         P.open(0, 50):
-            {'intv_ref': P.open(0, 100), 'ref': 'coding'},
+            {'reference_coordinate': P.open(0, 50), 'reference_type': 'coding'},
         P.open(40, 90):
-            {'intv_ref': P.open(0, 100), 'ref': 'coding'},
+            {'reference_coordinate': P.open(40, 90), 'reference_type': 'coding'},
         P.open(200, 250):
-            {'intv_ref': P.open(200, 300), 'ref': 'coding'},
+            {'reference_coordinate': P.open(200, 250), 'reference_type': 'coding'},
         P.open(220, 270):
-            {'intv_ref': P.open(200, 300), 'ref': 'coding'},
+            {'reference_coordinate': P.open(220, 270), 'reference_type': 'non_coding'},
         P.open(400, 450):
-            {'intv_ref': P.open(400, 450), 'ref': 'non_coding'}  # Assuming no CDS overlap
+            {'reference_coordinate': P.open(400, 450), 'reference_type': 'non_coding'}  # Assuming no CDS overlap
     }
 
     assert counter_handler.build_reference_dictionary(
