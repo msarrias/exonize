700519926a5538fb9f68937dd0058b45012b7dd6
msarrias <marina.sarrias@math.su.se>
2024-01-10 16:23:13 +0100

    did some refactoring, modified the create_events_multigraph and get_events_tuples_from_multigraph functions, created the get_node_degree,  build_reference_type_dictionary, build_event_coordinates_dictionary, assign_cluster_ids_to_event_coordinates, map_edges_to_records, build_events_list functions in the counter_handler.py scripts

--- exonize/counter_handler.py
+++ exonize/counter_handler.py
@@ -103,9 +103,10 @@ class CounterHandler(object):
             return max(cand_reference_list, key=lambda x: x[1])[0]
         return P.open(0, 0)
 
-    @staticmethod
     def get_overlapping_clusters(
-            target_coordinates_set: set[tuple[P.Interval, float]]
+            self,
+            target_coordinates_set: set[tuple[P.Interval, float]],
+            threshold: float = self.cds_overlapping_threshold
     ) -> list[list[tuple]]:
         """
         Get overlapping clusters of target coordinates.
@@ -126,7 +127,8 @@ class CounterHandler(object):
                     if (target_coordinate != other_coordinate and
                             self.two_way_overlapping(
                                 intv_i=target_coordinate,
-                                intv_j=other_coordinate
+                                intv_j=other_coordinate,
+                                threshold=threshold
                             )):
                         cluster.append((other_coordinate, other_evalue))
                         processed_intervals.add(other_coordinate)
@@ -134,7 +136,6 @@ class CounterHandler(object):
                 overlapping_clusters.append(cluster)
                 processed_intervals.add(target_coordinate)
 
-        # Optionally, sort clusters by their size in descending order
         overlapping_clusters.sort(key=len, reverse=True)
         return overlapping_clusters
 
@@ -165,8 +166,8 @@ class CounterHandler(object):
             for target_coordinate, _ in coordinates_cluster:
                 if candidate_reference:
                     reference_dictionary[target_coordinate] = {
-                        'intv_ref': candidate_reference,
-                        'ref': ref_type
+                        'reference_coordinate': candidate_reference,
+                        'reference_type': ref_type
                     }
                 else:
                     # Process separately if no shared reference
@@ -175,84 +176,177 @@ class CounterHandler(object):
                         overlapping_coordinates_list=[(target_coordinate, _)]
                     )
                     reference_dictionary[target_coordinate] = {
-                        'intv_ref': individual_reference if individual_reference else target_coordinate,
-                        'ref': 'coding' if individual_reference else 'non_coding'
+                        'reference_coordinate': individual_reference if individual_reference else target_coordinate,
+                        'reference_type': 'coding' if individual_reference else 'non_coding'
                     }
         return reference_dictionary
 
     @staticmethod
     def create_events_multigraph(
             reference_coordinates_dictionary: dict,
-            query_coords_set: set,
-            records_set: set,
+            query_coordinates_set: set,
+            tblastx_records_set: set,
     ) -> nx.MultiGraph:
-        gene_G = nx.MultiGraph()
-        target_coords_set = set([i['intv_ref'] for i in reference_coordinates_dictionary.values()])
-        gene_G.add_nodes_from(
-            set([(i.lower, i.upper)
-                 for i in [*query_coords_set, *target_coords_set]
+        gene_graph = nx.MultiGraph()
+        target_coordinates_set = set([
+            reference['reference_coordinate']
+            for reference in reference_coordinates_dictionary.values()
+        ])
+        gene_graph.add_nodes_from(
+            set([(node_coordinate.lower, node_coordinate.upper)
+                 for node_coordinate in [*query_coordinates_set, *target_coordinates_set]
                  ])
         )
-        for event in records_set:
-            source = (event[2], event[3])  # exact CDS coordinates
-            target = reference_coordinates_dictionary[P.open(event[4], event[5])]['intv_ref']  # we need a reference
-            ref_des = reference_coordinates_dictionary[P.open(event[4], event[5])]['ref']
-            gene_G.add_edge(
-                source,
-                (target.lower, target.upper),
-                fragment_id=event[0],
-                query_CDS=(event[2], event[3]),
-                target=(event[4], event[5]),
-                evalue=event[6],
-                event_type=event[7],
-                ref=ref_des,
+        for event in tblastx_records_set:
+            (fragment_id, _, source_start, source_end,
+             target_start, target_end, evalue, event_type) = event[:7]
+            target_coordinate = P.open(target_start, target_end)  # exact target coordinates
+            reference_type = reference_coordinates_dictionary[target_coordinate]['reference_type']
+            gene_graph.add_edge(
+                u_for_edge=(source_start, source_end),
+                v_for_edge=(target_start, target_end),
+                fragment_id=fragment_id,
+                query_CDS=(source_start, source_end),
+                target=(target_start, target_end),
+                evalue=evalue,
+                event_type=event_type,
+                reference_type=reference_type,
                 color='black',
                 width=2
             )
-        return gene_G
+        return gene_graph
+
+    @staticmethod
+    def get_node_degree(
+            graph: nx.MultiGraph,
+            node_coordinate_start: int,
+            node_coordinate_end: int
+    ) -> int:
+        return sum(1 for _ in graph.neighbors(
+            (node_coordinate_start, node_coordinate_end))
+                   )
+
+    @staticmethod
+    def build_reference_type_dictionary(
+            reference_coordinates_dictionary: dict
+    ) -> dict:
+        return {
+            reference_coordinate['reference_coordinate']: reference_coordinate['reference_type']
+            for reference_coordinate in reference_coordinates_dictionary.values()
+        }
+
+    @staticmethod
+    def build_event_coordinates_dictionary(
+            component: set[tuple],
+            reference_type_dict: dict,
+            gene_graph: nx.MultiGraph
+    ):
+        event_coord_dict = {}
+        for start, end in component:
+            node_coord = P.open(int(start), int(end))
+            ref_type = reference_type_dict.get(node_coord, 'coding')
+            degree = gene_graph.degree((start, end))
+            event_coord_dict[node_coord] = [ref_type, degree, None]  # cluster_id is None initially
+        return event_coord_dict
+
+    def assign_cluster_ids_to_event_coordinates(
+            self,
+            event_coordinates_dictionary: dict
+    ) -> dict[dict]:
+        # Convert event coordinates to a set of tuples for clustering
+        node_coordinates_set = set((node_coordinate, 0)
+                                   for node_coordinate in event_coordinates_dictionary.keys()
+                                   )
+        # get clusters of overlapping nodes
+        node_coordinates_clusters = [
+            [node_coordinate for node_coordinate, _ in cluster]
+            for cluster in self.get_overlapping_clusters(
+                target_coordinates_set=node_coordinates_set,
+                threshold=0  # we want to get all overlaps regardless of the percentage
+            )
+            if len(cluster) > 1
+        ]
+        cluster_id = 0
+        # Assign cluster id to each cluster within the component
+        for cluster in node_coordinates_clusters:
+            for node_coordinate in cluster:
+                event_coordinates_dictionary[node_coordinate][2] = cluster_id
+            cluster_id += 1
+        return event_coordinates_dictionary
+
+    @staticmethod
+    def map_edges_to_records(
+            graph: nx.MultiGraph,
+            event_id_counter: int,
+            component: set[tuple]
+    ) -> list[tuple]:
+        comp_event_list = []
+        subgraph = graph.subgraph(component)
+        # in here we are mapping the event id to each fragment id (event)
+        # where each fragment represents a BLAST hit between the query and the target
+        # hence, in the graph each fragment is represented by an edge
+        for edge in subgraph.edges(data=True):
+            # edge is a tuple (node1, node2, attributes)
+            comp_event_list.append(
+                (event_id_counter, edge[-1]['fragment_id'])
+            )
+        return comp_event_list
+
+    @staticmethod
+    def build_events_list(
+            gene_id: str,
+            event_coordinates_dictionary: dict
+    ) -> list[tuple]:
+        return [
+            (gene_id,
+             node_attributes[0],
+             node_coordinate.lower,
+             node_coordinate.upper,
+             *node_attributes[1:]
+             )
+            for node_coordinate, node_attributes in event_coordinates_dictionary.items()
+        ]
 
     def get_events_tuples_from_multigraph(
             self,
             reference_coordinates_dictionary: dict,
             gene_id: str,
             gene_graph: nx.MultiGraph
-    ) -> (list[tuple], set[tuple]):
-        reference = {i['intv_ref']: i['ref'] for i in reference_coordinates_dictionary.values()}
+    ):
+        reference_type_dictionary = self.build_reference_type_dictionary(
+            reference_coordinates_dictionary=reference_coordinates_dictionary
+        )
+        # each disconnected component will represent a duplication event within a gene
+        # and the number of nodes in the component is the number of duplicated exons
+        # i.e., each event is described by a number of duplicated exons
         disconnected_components = list(nx.connected_components(gene_graph))
-        events_tuples, events_list, event_id_counter, cluster_counter = list(), list(), 0, 0
+        events_tuples, events_list = [], []
+        event_id_counter = 0
         for component in disconnected_components:
-            temp, comp_event_list = dict(), list()
-            for node_x, node_y in component:
-                ref = 'coding'
-                node_intv = P.open(int(node_x), int(node_y))
-                if node_intv in reference:
-                    ref = reference[node_intv]
-                temp[node_intv] = [ref,  # either coding/non_coding/coding_non_coding
-                                   sum(1 for _ in gene_graph.neighbors((node_x, node_y))),  # degree
-                                   None,  # cluster_id
-                                   event_id_counter]
-            temp_list_tuples = set((node, 0) for node in temp.keys())
-            node_clusters = [[i[0] for i in cluster] for cluster in
-                             self.get_overlapping_clusters(
-                                 target_coordinates_set=temp_list_tuples
-                             )
-                             if len(cluster) > 1]
-            for cluster in node_clusters:
-                for node in cluster:
-                    temp[node][2] = cluster_counter
-                cluster_counter += 1
-            subgraph = gene_graph.subgraph(component)
-            for edge in subgraph.edges(data=True):
-                # edge is a tuple (node1, node2, attributes)
-                node1, node2, attributes = edge
-                comp_event_list.append(
-                    (event_id_counter, attributes['fragment_id'])
-                )
-            event_id_counter += 1
+            # First: Assign event id to each component
+            event_coordinates_dictionary = self.build_event_coordinates_dictionary(
+                component=component,
+                reference_type_dict=reference_type_dictionary,
+                gene_graph=gene_graph
+            )
+            # Second : Assign cluster ids within events
+            event_coordinates_dictionary = self.assign_cluster_ids_to_event_coordinates(
+                event_coordinates_dictionary=event_coordinates_dictionary
+            )
+            # Third: map each BLAST hit to an event id
+            comp_event_list = self.map_edges_to_records(
+                graph=gene_graph,
+                event_id_counter=event_id_counter,
+                component=component
+            )
             events_tuples.extend(comp_event_list)
-            events_list.extend([
-                (gene_id, value[0], coord.lower, coord.upper, *value[1:])
-                for coord, value in temp.items()])
+            events_list.extend(
+                self.build_events_list(
+                    gene_id=gene_id,
+                    event_coordinates_dictionary=event_coordinates_dictionary
+                )
+            )
+            event_id_counter += 1
         return events_tuples, events_list
 
     def assign_event_ids(
@@ -292,8 +386,8 @@ class CounterHandler(object):
             )
             G = self.create_events_multigraph(
                 reference_coordinates_dictionary=reference_coordinates_dictionary,
-                query_coords_set=query_coordinates,
-                records_set=records
+                query_coordinates_set=query_coordinates,
+                tblastx_records_set=records
             )
             gene_events_tuples, gene_events_set = self.get_events_tuples_from_multigraph(
                 reference_coordinates_dictionary=reference_coordinates_dictionary,
