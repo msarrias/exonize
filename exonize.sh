c0de97f1fb97cae5fba1c6409fbf1f0a3c81519a
msarrias <marina.sarrias@math.su.se>
2024-03-09 11:48:21 +0100

    moved bash script

--- exonize.sh
+++ exonize.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 GFF_FILE_PATH=data/bonobo/bonobo_first1081lines.gff
-GENOME_FILE_PATH=data/bonobo/bonobo.fa
+GENOME_FILE_PATH=data/bonobo/Pan_paniscus.panpan1.1.dna_rm.toplevel.fa
 SPECIE_ID=bonobo
 MIN_SEQ_LENGTH=20
-OUTPUT_DIRECTORY_PATH=/Users/marinaherrerasarrias/Desktop
+OUTPUT_DIRECTORY_PATH=.
 exonize $GFF_FILE_PATH \
         $GENOME_FILE_PATH \
         $SPECIE_ID \
