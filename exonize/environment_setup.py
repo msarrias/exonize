db70648197bb766dc0db624e00dab8441bab1c77
msarrias <marina.sarrias@math.su.se>
2024-04-22 16:26:07 +0200

    removed __FILE_ONLY_INFO setup from logger, kept only the console handler

--- exonize/environment_setup.py
+++ exonize/environment_setup.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import shutil
-from datetime import datetime
 
 
 class EnvironmentSetup(object):
@@ -29,45 +28,16 @@ class EnvironmentSetup(object):
         INFO level is used for the log file and WARNING and ERROR
         level for the console.
         """
-        logging.addLevelName(
-            self.__FILE_ONLY_INFO,
-            levelName='FILE_ONLY_INFO'
-        )
-        logging.Logger.file_only_info = self.file_only_info
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
-
-        # Define a filter that allows all messages EXCEPT those at INFO level
-        class ExcludeInfoFilter(logging.Filter):
-            def filter(self, record):
-                return record.levelno != logging.INFO
-
-        # Define file handler for the "FILE_ONLY_INFO" level
-        log_file_name = f"exonize_log_{datetime.now():%Y%m%d_%H%M%S}.log"
-        log_file_path = os.path.join(self.working_directory, log_file_name)
-        file_handler = logging.FileHandler(log_file_path, mode='w')
-        file_handler.setLevel(self.__FILE_ONLY_INFO)  # Changed level to custom level
-        file_handler.addFilter(ExcludeInfoFilter())  # Added filter
-        file_handler.setFormatter(logging.Formatter('%(message)s'))
-
         # Define console handler for the "INFO" level and above ("DEBUG", "WARNING", "EXCEPTION").
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
         console_handler.setFormatter(logging.Formatter('[%(levelname)s]: %(message)s'))
 
-        # Add handlers to the logger
-        self.logger.addHandler(file_handler)
-        self.logger.addHandler(console_handler)
-
-    # noinspection PyProtectedMember
-    def file_only_info(
-            self,
-            message,
-            *args,
-            **kws
-    ):
-        if self.logger.isEnabledFor(self.__FILE_ONLY_INFO):
-            self.logger._log(self.__FILE_ONLY_INFO, message, args, **kws)
+        logging.basicConfig(
+            level=logging.INFO, handlers=[console_handler]
+        )
 
     def setup_environment(self):
         if self.HARD_FORCE:
