30fe41753354c40dd4fe1a0aba9c64d3a1ef2809
msarrias <marina.sarrias@math.su.se>
2024-04-22 19:25:29 +0200

    using Y chromosome as test case

--- test_human_ch21/fetch_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -O https://ftp.ensembl.org/pub/release-111/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.21.fa.gz
-curl -O https://ftp.ensembl.org/pub/release-111/gff3/homo_sapiens/Homo_sapiens.GRCh38.111.chromosome.21.gff3.gz
-gunzip *.gz
