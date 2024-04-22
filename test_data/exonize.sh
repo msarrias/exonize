3886b9ac7d372b606c2bca0ac5009d11dd4516ae
msarrias <marina.sarrias@math.su.se>
2024-04-22 15:09:31 +0200

    added script for running test case

--- test_data/exonize.sh
+++ test_data/exonize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-GFF_FILE_PATH=data/bonobo/bonobo_first1081lines.gff
-GENOME_FILE_PATH=data/bonobo/Pan_paniscus.panpan1.1.dna_rm.toplevel.fa
-SPECIE_ID=bonobo
-MIN_SEQ_LENGTH=20
+GFF_FILE_PATH=Homo_sapiens.GRCh38.111.chromosome.21.gff3
+GENOME_FILE_PATH=Homo_sapiens.GRCh38.dna.chromosome.21.fa
+SPECIE_ID=Homo_sapiens_ch21
+MIN_SEQ_LENGTH=30
 OUTPUT_DIRECTORY_PATH=.
 exonize $GFF_FILE_PATH \
         $GENOME_FILE_PATH \
