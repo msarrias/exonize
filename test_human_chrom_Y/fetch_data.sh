433b8d32d72f097e19a5101c1c38dca24d3b89ab
msarrias <marina.sarrias@math.su.se>
2024-05-23 19:47:26 +0200

    test case: download the latest human annotations

--- test_human_chrom_Y/fetch_data.sh
+++ test_human_chrom_Y/fetch_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -O https://ftp.ensembl.org/pub/release-111/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.Y.fa.gz
-curl -O https://ftp.ensembl.org/pub/release-111/gff3/homo_sapiens/Homo_sapiens.GRCh38.111.chromosome.Y.gff3.gz
+curl -O https://ftp.ensembl.org/pub/release-111/gff3/homo_sapiens/Homo_sapiens.GRCh38.112.chromosome.Y.gff3.gz
 gunzip *gff3.gz
