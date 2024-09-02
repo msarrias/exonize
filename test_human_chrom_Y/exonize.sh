c5307f5d2e688845da9c083532406d58621b2824
msarrias <marina.sarrias@math.su.se>
2024-09-02 16:21:38 +0900

    removed unused variable

--- test_human_chrom_Y/exonize.sh
+++ test_human_chrom_Y/exonize.sh
@@ -5,7 +5,6 @@ GENOME_FILE_PATH=Homo_sapiens.GRCh38.dna.chromosome.Y.fa.gz
 SPECIE_ID=Homo_sapiens_chrom_Y
 EVALUE=1e-3
 MIN_SEQ_LENGTH=30
-OUTPUT_DIRECTORY_PATH=.
 exonize $GFF_FILE_PATH \
         $GENOME_FILE_PATH \
         --output_prefix $SPECIE_ID \
