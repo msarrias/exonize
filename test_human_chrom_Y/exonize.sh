1546c75001779d4af032c31f178604f008185712
msarrias <marina.sarrias@math.su.se>
2024-09-11 14:48:48 +0900

    adapted tests to new flags

--- test_human_chrom_Y/exonize.sh
+++ test_human_chrom_Y/exonize.sh
@@ -4,13 +4,17 @@ GFF_FILE_PATH=Homo_sapiens.GRCh38.111.chromosome.Y.gff3
 GENOME_FILE_PATH=Homo_sapiens.GRCh38.dna.chromosome.Y.fa.gz
 SPECIE_ID=Homo_sapiens_chrom_Y
 EVALUE=1e-3
+CDS_ANNOT='CDS'
+GENE_ANNOT='gene'
+TRANS_ANNOT='mRNA'
 MIN_SEQ_LENGTH=30
 exonize $GFF_FILE_PATH \
         $GENOME_FILE_PATH \
+        --gene-annot-feature $GENE_ANNOT \
+        --cds-annot-feature $CDS_ANNOT \
+        --transcript-annot-feature $TRANS_ANNOT \
         --output_prefix $SPECIE_ID \
         --min-exon-length $MIN_SEQ_LENGTH \
         --evalue-threshold $EVALUE \
-        --multigraphs \
         --csv \
-        --cpus_number 5 \
 
