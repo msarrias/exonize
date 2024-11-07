a1485a599d63c929366433343aa37eab8e56e94d
msarrias <marina.sarrias@math.su.se>
2024-11-07 16:58:17 +0100

    added documentation

--- exonize/exonize_analysis.py
+++ exonize/exonize_analysis.py
@@ -7,6 +7,24 @@ from pathlib import Path
 
 
 class Gene:
+    """
+    Gene class is a container for gene expansion graphs.
+
+    Attributes
+    ----------
+    id : str
+        The unique identifier for the gene.
+    coordinates : tuple
+        The start and end coordinates of the gene on the chromosome.
+    strand : str
+        The DNA strand ('+' or '-') on which the gene is located.
+    chromosome : str
+        The chromosome on which the gene is located.
+    expansions : dict
+        A dictionary where keys are expansion IDs and values are expansion objects, each containing an expansion graph.
+    plot_handler : PlotHandler
+        An instance of PlotHandler responsible for visualizing the gene's expansions.
+    """
     def __init__(
             self,
             gene_id,
@@ -14,6 +32,20 @@ class Gene:
             strand,
             chromosome
     ):
+        """
+        Initializes a Gene instance.
+
+        Parameters
+        ----------
+        gene_id : str
+            The unique identifier for the gene.
+        coordinates : tuple
+            The start and end coordinates of the gene on the chromosome.
+        strand : str
+            The DNA strand ('+' or '-') on which the gene is located.
+        chromosome : str
+            The chromosome on which the gene is located.
+        """
         self.id = gene_id
         self.coordinates = coordinates
         self.strand = strand
@@ -22,18 +54,88 @@ class Gene:
         self.plot_handler = PlotHandler()
 
     def __getitem__(self, expansion_id):
+        """
+        Retrieves the expansion graph for the specified expansion ID.
+
+        Parameters
+        ----------
+        expansion_id : int
+            The ID of the expansion to retrieve.
+
+        Returns
+        -------
+        networkx.Graph
+            The expansion graph associated with the given expansion ID.
+
+        Examples
+        --------
+        >>> gene[1]  # Retrieves the expansion graph for expansion ID 1
+        """
         return self.expansions[expansion_id].graph
 
     def __iter__(self):
+        """
+        Returns an iterator over the expansion graphs.
+
+        Returns
+        -------
+        iterator
+            An iterator yielding each expansion graph.
+
+        Examples
+        --------
+        >>> for graph in gene:
+        ...     print(graph)
+        """
         return iter(expansion.graph for expansion in self.expansions.values())
 
     def __repr__(self):
+        """
+        Returns a string representation of the Gene object.
+
+        Returns
+        -------
+        str
+            A string describing the gene's ID and number of expansions.
+
+        Examples
+        --------
+        >>> repr(gene)
+        '<Gene GENE123 with 0 expansions (iterable of expansion graphs)>'
+        """
         return f"<Gene {self.id} with {len(self.expansions)} expansions (iterable of expansion graphs)>"
 
     def __len__(self):
+        """
+        Returns the number of expansions associated with the gene.
+
+        Returns
+        -------
+        int
+            The number of expansions.
+
+        Examples
+        --------
+        >>> len(gene)
+        0
+        """
         return len(self.expansions)
 
     def build_gene_graph(self):
+        """
+        Builds and returns a consolidated gene graph containing nodes and edges from all expansion graphs.
+
+        Returns
+        -------
+        networkx.Graph
+            A combined graph with nodes and edges from all expansions.
+
+        Examples
+        --------
+        >>> combined_graph = gene.build_gene_graph()
+        >>> print(combined_graph.nodes)
+        >>> print(combined_graph.edges)
+        """
         gene_graph = nx.Graph(id=self.id)
         for expansion in self.expansions.values():
             for node, data in expansion.graph.nodes(data=True):
@@ -51,6 +153,25 @@ class Gene:
             connect_overlapping_nodes: bool = True,
             full_expansion: bool = False
     ):
+        """
+        Draws a multi-graph of gene expansions.
+
+        Parameters
+        ----------
+        expansion_id : int, optional
+            The ID of a specific expansion to draw. If None, the combined gene graph is drawn.
+        figure_path : Path, optional
+            The path to save the figure. If None, the figure is not saved.
+        figure_size : tuple of float, optional
+            The size of the figure in inches. Default is (8.0, 8.0).
+        legend : bool, optional
+            Whether to display a legend on the plot. Default is True.
+        connect_overlapping_nodes : bool, optional
+            Whether to draw edges connecting overlapping nodes in the graph. Default is True.
+        full_expansion : bool, optional
+            Whether to show the full expansion graph only. Default is False.
+
+        """
         if expansion_id is not None:
             G = self.expansions[expansion_id].graph
         else:
@@ -67,12 +188,38 @@ class Gene:
 
 
 class Expansion:
+    """
+    Expansion class represents an expansion graph for a specific gene expansion.
+
+    Attributes
+    ----------
+    graph : networkx.Graph
+        A NetworkX graph representing the expansion.
+    """
     def __init__(
             self,
             expansion_id,
             nodes,
             edges
     ):
+        """
+        Initializes an Expansion instance.
+
+        Parameters
+        ----------
+        expansion_id : int
+            The unique identifier for the expansion.
+        nodes : list of tuples
+            A list of tuples representing the nodes, where each tuple is in the form (coord, node_type).
+            - coord: The coordinate of the node (e.g., an integer or other unique identifier).
+            - node_type: The type of the node (e.g., 'FULL', 'PARTIAL').
+        edges : list of tuples
+            A list of tuples representing the edges, where each tuple is in the form (query_coord, target_coord, mode).
+            - query_coord: The coordinate of the query node.
+            - target_coord: The coordinate of the target node.
+            - mode: The match mode.
+
+        """
         self.graph = nx.Graph()
         self.graph.id = expansion_id
         self.graph.add_nodes_from([
@@ -89,35 +236,150 @@ class Expansion:
 
 
 class GenomeExpansions:
+    """
+    GenomeExpansions class is a container for managing gene expansions across an entire genome.
+
+     Attributes
+    ----------
+    exonize_db_path : str
+        The file path to the Exonize database.
+    _genes : dict
+        A dictionary of Gene objects, keyed by gene ID.
+    _db_handler : ExonizeDBHandler
+        An instance of ExonizeDBHandler for interacting with the database.
+    """
     def __init__(
             self,
             exonize_db_path: str
     ):
+        """
+        Initializes a GenomeExpansions instance and builds expansions from the database.
+
+        Parameters
+        ----------
+        exonize_db_path : str
+            The file path to the Exonize database.
+        """
         self.exonize_db_path = exonize_db_path
         self._genes = {}
         self._db_handler = ExonizeDBHandler(self.exonize_db_path)
         self.build_expansions()
 
     def __iter__(self):
+        """
+        Returns an iterator over the Gene objects.
+
+        Returns
+        -------
+        iterator
+            An iterator yielding each Gene object.
+
+        Examples
+        --------
+        >>> for gene in genome_expansions:
+        ...     print(gene)
+        """
         return iter(self._genes.values())
 
     def __contains__(self, n):
+        """
+        Checks if a gene ID exists in the GenomeExpansions.
+
+        Parameters
+        ----------
+        n : str
+            The gene ID to check for existence.
+
+        Returns
+        -------
+        bool
+            True if the gene ID exists, False otherwise.
+
+        Examples
+        --------
+        >>> "GENE123" in genome_expansions
+        True
+        """
         return n in self._genes
 
     def __getitem__(self, gene_id):
+        """
+        Retrieves a Gene object by gene ID.
+
+        Parameters
+        ----------
+        gene_id : str
+            The ID of the gene to retrieve.
+
+        Returns
+        -------
+        Gene
+            The Gene object associated with the specified gene ID.
+
+        Examples
+        --------
+        >>> gene = genome_expansions["GENE123"]
+        >>> print(gene)
+        <Gene GENE123 with 0 expansions (iterable of expansion graphs)>
+        """
         return self._genes[gene_id]
 
     def __len__(self):
+        """
+        Returns the number of genes in the GenomeExpansions.
+
+        Returns
+        -------
+        int
+            The number of genes in the GenomeExpansions.
+
+        Examples
+        --------
+        >>> len(genome_expansions)
+        18
+        """
         return len(self._genes)
 
     @property
     def genes(self):
+        """
+        Returns a list of gene IDs.
+
+        Returns
+        -------
+        list
+            A list of gene IDs in the GenomeExpansions.
+
+        Examples
+        --------
+        >>> genome_expansions.genes
+        ['GENE123', 'GENE456', 'GENE789']
+        """
         return list(self._genes.keys())
 
     def add_gene(
             self,
             gene_id: str
     ) -> Gene:
+        """
+        Adds a Gene object to the genome based on the provided gene ID.
+
+        Parameters
+        ----------
+        gene_id : str
+            The unique identifier for the gene.
+
+        Returns
+        -------
+        Gene
+            The Gene object that was added.
+
+        Examples
+        --------
+        >>> gene = genome_expansions.add_gene("GENE123")
+        >>> print(gene)
+        <Gene GENE123 with 0 expansions (iterable of expansion graphs)>
+        """
         if gene_id not in self._genes:
             chrom, strand, start, end = self._db_handler.genes_dict[gene_id]
             return Gene(
@@ -128,6 +390,18 @@ class GenomeExpansions:
             )
 
     def build_expansions(self):
+        """
+        Constructs the expansion data for each gene by loading nodes and edges from the database.
+
+        This method initializes each Gene object and populates its expansions based on data from
+        the Exonize database. Each expansion consists of nodes and edges, forming a graph for each gene.
+
+        Examples
+        --------
+        >>> genome_expansions.build_expansions()
+        >>> print(len(genome_expansions))
+        18
+        """
         for gene_id, non_reciprocal_matches in self._db_handler.gene_expansions_dict.items():
             self._genes[gene_id] = self.add_gene(
                 gene_id=gene_id
@@ -144,7 +418,19 @@ class GenomeExpansions:
 
 
 class PlotHandler:
+    """
+    PlotHandler is a class for managing and visualizing gene expansion graphs
+    with various layout and styling options.
+
+    Attributes
+    ----------
+    _color_map : dict
+        A mapping of node types to specific colors for visualization.
+    """
     def __init__(self):
+        """
+        Initializes the PlotHandler instance with predefined node types and color mappings.
+        """
         self._full = 'FULL'
         self._partial_insertion = 'PARTIAL_INSERTION'
         self._partial_excision = 'PARTIAL_EXCISION'
@@ -163,6 +449,22 @@ class PlotHandler:
             components: list[list],
             gene_graph: nx.MultiGraph
     ):
+        """
+        Calculates positions for each component in the graph for plotting.
+
+        Parameters
+        ----------
+        components : list of lists
+            A list of connected components in the gene graph, where each component is a list of nodes.
+        gene_graph : nx.MultiGraph
+            The graph structure containing nodes and edges of the gene.
+
+        Returns
+        -------
+        dict
+            A dictionary mapping each node to a position (x, y) coordinate for visualization.
+
+        """
         layout_scale = 2
         component_positions = []
         large_components = [comp for comp in components if len(comp) > 2]
@@ -219,6 +521,14 @@ class PlotHandler:
     def full_expansion(
             graph: nx.Graph,
     ) -> None:
+        """
+        Removes nodes from the graph that are not labled as 'FULL' type.
+
+        Parameters
+        ----------
+        graph : nx.Graph
+
+        """
         nodes_to_drop = [
             node
             for node in graph.nodes
@@ -237,6 +547,26 @@ class PlotHandler:
             connect_overlapping_nodes: bool = True,
             full_expansion: bool = False
     ):
+        """
+        Draws a multi-graph of gene expansions.
+
+        Parameters
+        ----------
+        gene_start : int
+            The start position of the gene for labeling purposes.
+        gene_graph : nx.Graph or nx.MultiGraph
+            The graph containing nodes and edges of gene expansions.
+        figure_size : tuple of float, optional
+            The size of the figure in inches. Default is (8.0, 8.0).
+        figure_path : Path, optional
+            The path to save the figure. If None, the figure is not saved.
+        legend : bool, optional
+            Whether to display a legend on the plot. Default is True.
+        connect_overlapping_nodes : bool, optional
+            Whether to draw edges connecting overlapping nodes in the graph. Default is True.
+        full_expansion : bool, optional
+            Whether to show the full expansion graph only. Default is False.
+        """
         G = gene_graph.copy()
         if full_expansion:
             self.full_expansion(
@@ -341,6 +671,19 @@ class PlotHandler:
             self,
             gene_graph: nx.MultiGraph
     ):
+        """
+        Identifies and returns overlapping nodes in the gene graph for visualization.
+
+        Parameters
+        ----------
+        gene_graph : nx.MultiGraph
+            The graph containing nodes and edges of gene expansions.
+
+        Returns
+        -------
+        list of lists
+            A list of overlapping node pairs.
+        """
         overlapping_clusters = self._get_overlapping_clusters(
             target_coordinates_set=set([
                 (coordinate, None)
@@ -432,7 +775,32 @@ class PlotHandler:
 
 
 class ExonizeDBHandler:
+    """
+        ExonizeDBHandler class manages database interactions for gene and expansion data with exon duplications.
+
+        Attributes
+        ----------
+        db_path : str
+            Path to the SQLite exonize database file.
+        genes_dict : dict
+            Dictionary of gene information, with gene IDs as keys and tuples of
+            chromosome, strand, start, and end as values.
+        gene_expansions_dict : dict
+            Dictionary of expansions associated with each gene, including nodes and edges for each expansion.
+        _expansions_nodes : list
+            Expansion nodes fetched from the database.
+        _expansions_edges : set
+           Expansion edges fetched from the database.
+        """
     def __init__(self, db_path):
+        """
+        Initializes an ExonizeDBHandler instance, fetching genes and expansions from the database.
+
+        Parameters
+        ----------
+        db_path : str
+            Path to the SQLite database file.
+        """
         self.db_path = db_path
         self.genes_dict = self.collect_genes(self.fetch_genes_with_exon_dup())
         self.gene_expansions_dict = {}
@@ -441,7 +809,7 @@ class ExonizeDBHandler:
         self.collect_expansion_nodes(expansions=self._expansions_nodes)
         self.collect_expansions_edges(matches_set=self._expansions_edges)
 
-    def check_if_table_exists(
+    def _check_if_table_exists(
             self,
             table_name: str
     ) -> bool:
@@ -530,7 +898,7 @@ class ExonizeDBHandler:
         global_matches = set()
         with sqlite3.connect(self.db_path) as db:
             cursor = db.cursor()
-            if self.check_if_table_exists(
+            if self._check_if_table_exists(
                     table_name="Local_matches_non_reciprocal"
             ):
                 cursor.execute(
@@ -546,7 +914,7 @@ class ExonizeDBHandler:
                     """
                 )
                 local_matches = set(cursor.fetchall())
-            if self.check_if_table_exists(
+            if self._check_if_table_exists(
                     table_name="Global_matches_non_reciprocal"
             ):
                 cursor.execute(
