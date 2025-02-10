adc3c32c9f2c8621674d6de80a723eca5dc4b1bd
msarrias <marina.sarrias@math.su.se>
2025-02-10 14:47:59 +0100

    refacto handle_reruns to reduce complexity

--- exonize/data_preprocessor.py
+++ exonize/data_preprocessor.py
@@ -237,30 +237,56 @@ class DataPreprocessor(object):
                 f"There was an error reading the genome file: {e}"
             )
             sys.exit()
-
+    
+    def _check_basic_rerun_conditions(
+            self,
+            sequence_base: int,
+            frame_base: int,
+            min_exon_length: int,
+            exon_clustering_overlap_threshold: float):
+        return (self.environment.sequence_base != sequence_base
+                or self.environment.frame_base != frame_base
+                or self.environment.min_exon_length != min_exon_length
+                or self.environment.exon_clustering_overlap_threshold != exon_clustering_overlap_threshold)
+    
+    def _action_local_search(self, evalue_threshold: float):
+        if self.environment.evalue_threshold < evalue_threshold:
+            self.database_interface.drop_table(table_name='Local_search')
+            self.database_interface.clear_search_monitor_table(global_search=True)
+    
+    def _action_global_search(
+            self,
+            fraction_of_aligned_positions: float,
+            peptide_identity_threshold: float,
+            pair_coverage_threshold: float
+    ):
+        if (self.environment.fraction_of_aligned_positions != fraction_of_aligned_positions
+                or self.environment.peptide_identity_threshold != peptide_identity_threshold
+                or self.environment.pair_coverage_threshold != pair_coverage_threshold):
+            self.database_interface.drop_table(table_name='Global_search')
+            self.database_interface.clear_search_monitor_table(local_search=True)
+            
     def handle_reruns(self):
         if self.database_interface.check_if_table_exists(table_name='Parameter_monitor'):
             if not self.database_interface.check_if_empty_table(table_name='Parameter_monitor'):
                 (sb, fb, l, c_e,
                  t_e, e, t_s, c_t,
                  t_p, t_i, t_a) = self.database_interface.query_parameter_monitor_table()
-                if (self.environment.sequence_base != sb
-                        or self.environment.frame_base != fb
-                        or self.environment.min_exon_length != l
-                        or self.environment.exon_clustering_overlap_threshold != c_e):
+                if self._check_basic_rerun_conditions(sb, fb, l, c_e):
                     self.database_interface.clear_results_database(
                         except_tables=['Parameter_monitor']
                     )
                 if self.environment.SEARCH_ALL or self.environment.LOCAL_SEARCH:
-                    if self.environment.evalue_threshold < e:
-                        self.database_interface.drop_table(table_name='Local_search')
-                        self.database_interface.clear_search_monitor_table(global_search=True)
+                    self._action_local_search(
+                        evalue_threshold=e
+                    )
                 if self.environment.SEARCH_ALL or self.environment.GLOBAL_SEARCH:
-                    if (self.environment.fraction_of_aligned_positions != t_a
-                            or self.environment.peptide_identity_threshold != t_i
-                            or self.environment.pair_coverage_threshold != t_p):
-                        self.database_interface.drop_table(table_name='Global_search')
-                        self.database_interface.clear_search_monitor_table(local_search=True)
+                    self._action_global_search(
+                        fraction_of_aligned_positions=t_a,
+                        peptide_identity_threshold=t_i,
+                        pair_coverage_threshold=t_p
+                    )
+                    
                 self.database_interface.update_parameter_monitor()
             else:
                 self.database_interface.insert_parameter_monitor()
